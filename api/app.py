56950aae883f03fe46ac5e1e2cf28073b5530edf
Sanjeev S <sanjeev.s@flipkart.com>
2018-06-14 22:08:15 +0530

    remove cors

--- api/app.py
+++ api/app.py
@@ -1,10 +1,8 @@
 from flask import Flask, request, jsonify, send_from_directory
 from in_memory_db import InMemoryDB
-from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app)
 db = InMemoryDB()
 
 
